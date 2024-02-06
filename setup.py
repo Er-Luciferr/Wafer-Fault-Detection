e31d370c8f43ca0f39e0cef5c55849533b0bbdf8
Er-Luciferr <mohitsingh2222.ms@gmail.com>
2024-02-06 17:04:16 +0530

    demo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='Wafer Fault Dectection',
+    version='0.0.1',
+    author='Mohit Singh',
+    author_email="mohitsingh2@yahoo.com",
+    packages=find_packages(),
+    install_requires=['requirements.txt']
+)   
+
+
